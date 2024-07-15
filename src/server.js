81d0de2e1cdd4c804576c03379ad6978b43e8fb4
Mr_Veit <Misster.veit@gmail.com>
2024-07-16 11:20:03 +1200

    Base update

---
+++ src/server.js
@@ -0,0 +1,37 @@
+const time = require('./timeUtils');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+require('dotenv').config(); 
+
+const app = express();
+const port = process.env.PORT || 1000;
+
+let latestPaymentDataFromBot = null;
+
+app.use(bodyParser.json());
+
+app.post('/api/payment/process', (request, result) => 
+{
+    latestPaymentDataFromBot = request.body;
+
+    console.log(`[${time.timestamp}] Received payment data:`, latestPaymentDataFromBot);
+
+    result.sendStatus(200);
+});
+
+app.get('/api/payment/get-info', (request, result) => 
+{
+    if (!latestPaymentDataFromBot)
+    {
+        return result.status(404).json({ error: 'Bot payment data not found, please try again later!'});
+    }
+
+    result.status(200).json(latestPaymentDataFromBot);
+});
+
+app.listen(port, () => 
+{
+    console.log(`Server running at ${process.env.SERVER_DOMAIN}:${port}`);
+});
