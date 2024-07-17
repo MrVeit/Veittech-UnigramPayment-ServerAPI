46510d5d5d0cc636b076a6f1dcef8bf9c75de6d8
Mr_Veit <Misster.veit@gmail.com>
2024-07-18 07:50:12 +1200

    Added authorization of Unity client and Telegram bot to avoid unauthorized API access and more
    Added method for requesting refund of purchased stars.

---
+++ src/paymentAPI.js
@@ -0,0 +1,94 @@
+const time = require('./timeUtils');
+
+const axios = require('axios');
+
+require('dotenv').config();
+
+const createInvoiceLink = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/createInvoiceLink`;
+const refundPaymentLink = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/refundStarPayment`;
+
+async function createInvoice(request, result)
+{
+    var productData = createProductData(request);
+
+    try
+    {
+        const response = await axios.post(createInvoiceLink, productData);
+
+        if (response.data && response.data.result)
+        {
+            result.status(200).json({ invoiceLink: response.data.result });
+        }
+        else
+        {
+            result.status(500).json({ error: 'Failed to create invoice link.'});
+        }
+    }
+    catch (error)
+    {
+        console.error(`[${time.timestamp}] Error creating invoice:`, error);
+
+        result.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+async function refund(request, result)
+{
+    var refundData = createRefundData(request);
+
+    try 
+    {
+        const response = await axios.post(refundPaymentLink, refundData);
+
+        if (response.data && response.data.ok)
+        {
+            result.status(200).json({ message: 'The refund to the buyer was successfully made'})
+        }
+        else
+        {
+            result.status(500).json({ error: 'Failed to process refund' });
+        }
+    }
+    catch (error)
+    {
+        console.error(`[${time.timestamp}] Error processing refund:`, error);
+
+        result.status(500).json({ error: 'Internal Server Error' });
+    }
+}
+
+function createProductData(request)
+{
+    const { title, description, payload, providerToken, currency, amount } = request.body;
+
+    const data =
+    {
+        title,
+        description,
+        payload,
+        provider_token: providerToken,
+        currency,
+        prices: [{ label: title, amount }]
+    };
+
+    return data;
+}
+
+function createRefundData(request)
+{
+    const { userId, transactionId } = request.body;
+
+    const data = 
+    {
+        user_id: userId,
+        telegram_payment_charge_id: transactionId
+    };
+
+    return data;
+}
+
+module.exports = 
+{
+    createInvoice,
+    refund
+};
