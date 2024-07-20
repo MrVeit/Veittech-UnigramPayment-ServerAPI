c951dfe30347ef7a5e46146a9133e15dde495f47
Mr_Veit <Misster.veit@gmail.com>
2024-07-20 14:50:01 +1200

    Removed the CORS library due to the ability to customize the required headers through the Nginx/Apache web server

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ const payment = require('./paymentAPI');
 const time = require('./timeUtils');
 
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 
 require('dotenv').config();
@@ -13,7 +12,6 @@ const port = process.env.PORT || 1000;
 
 let latestPaymentDataFromBot = null;
 
-app.use(cors);
 app.use(bodyParser.json());
 
 app.post('/api/authenticate', bodyParser.text(), (request, result) => 
